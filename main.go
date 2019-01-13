771e4847bbaf73a20261ac106756959abea8813c
Aaron Paz <aaron@polyverse.io>
2019-01-13 11:36:52 -0800

    fix main.go

--- main.go
+++ main.go
@@ -3,9 +3,25 @@ package main
 import (
 	"github.com/apaz037/go-compiler/lexer"
 	"github.com/apaz037/go-compiler/parser"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
-func must (err error) {
+func main() {
+	if len(os.Args) < 2 {
+		panic("no valid file name or path provided for file!")
+	}
+
+	path := os.Args[1]
+	absPath, _ := filepath.Abs(path)
+	contents, err := ioutil.ReadFile(absPath)
+	check(err)
+	Parse(string(contents))
+}
+
+
+func check (err error) {
 	if err != nil {
 		panic(err)
 	}
@@ -15,4 +31,6 @@ func Parse(input string) {
 	l := lexer.NewLexer([]byte(input))
 	p := parser.NewParser()
 
+	_, err := p.Parse(l)
+	check(err)
 }
